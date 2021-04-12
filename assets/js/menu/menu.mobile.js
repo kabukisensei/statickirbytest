ddd375e8098cf745969e545304ada8da25c3ab23
Aaron Jennings <taoistwolf@gmail.com>
2021-04-12 14:15:28 -0500

    Delete assets/js directory

--- assets/js/menu/menu.mobile.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.navbar-link').forEach(function(navbarLink){
-    navbarLink.addEventListener('click', function(){
-      navbarLink.nextElementSibling.classList.toggle('is-hidden-touch');
-    })
-  });
